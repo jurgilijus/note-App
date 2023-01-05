917bf13772c2f0ae0ba73bf92118eec6d460ba6e
Jurgis Subačius <38658881+jurgilijus@users.noreply.github.com>
2023-01-05 11:13:28 +0200

    registration layout is done

--- src/Components/Regitration/Registration.jsx
+++ src/Components/Regitration/Registration.jsx
@@ -1,7 +1,23 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
+// CSS
+import "./Registration.css";
 
 function Registration() {
-  return <div>Registration</div>;
+  return (
+    <section className="registration-conteiner">
+      <form className="registration-form">
+        <h2>Register</h2>
+        <input type="email" placeholder="Your e-mail.." />
+        <input type="password" placeholder="Your password.." />
+        <button className="login-btn">Register</button>
+        <Link to="/" className="login-link">
+          Have an account?
+        </Link>
+      </form>
+    </section>
+  );
 }
 
 export default Registration;
