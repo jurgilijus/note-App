25c870259608a520c79a7e8e420bd39608844dd7
Jurgis Subačius <38658881+jurgilijus@users.noreply.github.com>
2023-01-10 16:59:08 +0200

    test

--- src/Components/Notes/NotePopup/NotePopup.jsx
+++ src/Components/Notes/NotePopup/NotePopup.jsx
@@ -53,7 +53,7 @@ function NotePopup({ note, hide }) {
               //   setDate({ title: e.target.value });
               // }}
             />
-
+            <p>Labas</p>
             <button
               title="edit note"
               className="edit-btn"
